babecc1585f574f607d645537fc6ac8d365a4e8b
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-10-19 03:16:05 +0300

    Add express app code skeleton

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log("Hello, TS");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_errors_1 = __importDefault(require("http-errors"));
+const morgan_1 = __importDefault(require("morgan"));
+const config_1 = __importDefault(require("./config"));
+const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
+const comments_routes_1 = __importDefault(require("./routes/comments.routes"));
+const posts_routes_1 = __importDefault(require("./routes/posts.routes"));
+const topics_routes_1 = __importDefault(require("./routes/topics.routes"));
+const users_routes_1 = __importDefault(require("./routes/users.routes"));
+// Initialize Application
+const app = (0, express_1.default)();
+// Use Logger
+app.use((0, morgan_1.default)('dev'));
+// Set Routes
+app.use('/', auth_routes_1.default);
+app.use('/auth', auth_routes_1.default);
+app.use('/comments', comments_routes_1.default);
+app.use('/posts', posts_routes_1.default);
+app.use('/topics', topics_routes_1.default);
+app.use('/users', users_routes_1.default);
+// 404 Handler
+app.use((req, res, next) => {
+    next((0, http_errors_1.default)(404, "Resource Not Found"));
+});
+// Errors Handler
+app.use((err, req, res, next) => {
+    res.status(err.statusCode).json({
+        success: false,
+        statusCode: err.statusCode,
+        error: err
+    });
+});
+app.listen(config_1.default.port, () => console.log(`Listening on port ${config_1.default.port}`));
+exports.default = app;
--- index.ts
+++ index.ts
@@ -1,9 +1,43 @@
-import { PrismaClient } from '@prisma/client';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import createError, { HttpError } from 'http-errors';
+import morgan from 'morgan';
 
-const prisma = new PrismaClient();
+import config from './config';
 
-prisma.user.findMany().then(users => {
-    console.log(users);
+import authRouter from './routes/auth.routes';
+import commentsRouter from './routes/comments.routes';
+import postsRouter from './routes/posts.routes';
+import topicsRouter from './routes/topics.routes';
+import usersRouter from './routes/users.routes';
+
+// Initialize Application
+const app: Express = express();
+
+// Use Logger
+app.use(morgan('dev'));
+
+// Set Routes
+app.use('/', authRouter);
+app.use('/auth', authRouter);
+app.use('/comments', commentsRouter);
+app.use('/posts', postsRouter);
+app.use('/topics', topicsRouter);
+app.use('/users', usersRouter);
+
+// 404 Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(createError(404, "Resource Not Found"));
 })
 
-console.log("Hello, TS");
+// Errors Handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction): void => {
+    res.status(err.statusCode).json({
+        success: false,
+        statusCode: err.statusCode,
+        error: err
+    });
+})
+
+app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
+
+export default app;
